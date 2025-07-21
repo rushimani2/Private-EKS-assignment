496935e94cfd20dcb6778865f31d0388e5b2042e
Rushimanikanata Lingam <158359799+rushimani2@users.noreply.github.com>
2025-07-21 13:59:53 +0530

    Update get-nodes.sh

--- get-nodes.sh
+++ get-nodes.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-# Script to list EKS nodes with their internal IP addresses
-
-# Check if kubectl is installed
-if ! command -v kubectl &> /dev/null; then
-  echo "kubectl is not installed. Please install it before running this script."
-  exit 1
-fi
-
-# List nodes with internal IPs
-echo -e "\nNode Name\t\tInternal IP"
+echo -e "Node Name\t\tInternal IP"
 echo "-------------------------------"
-kubectl get nodes -o wide | awk 'NR==1 || /ip-/{printf "%s\t%s\n", $1, $6}'
+
+kubectl get nodes -o json | jq -r '.items[] | [.metadata.name, (.status.addresses[] | select(.type=="InternalIP").address)] | @tsv'
